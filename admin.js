f67589e44600fd2fe23c9c4181e0c5a5e181f80b
Javad <168000154+hello23445@users.noreply.github.com>
2026-02-01 16:37:22 +0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -6,8 +6,9 @@ const ADMIN_TOKENS = ['FQ87GDWKJSLLUVOA', ''];
 const disableCreateAds = '';// '' или 'disabled'
 const GASES = '';//если no то блокируем обращения к GAS
 const closeApp = '';// '' или 'closed'
-const PURCHASES = 'no'; // '' или 'no' — если 'no' тогда отключаем платежи
+const PURCHASES = ''; // '' или 'no' — если 'no' тогда отключаем платежи
 
 // Массивы для блокировки пользователей - используйте ТОКЕНЫ пользователей
 const banForeverAds = ['']; // Токены пользователей, которым запрещено создавать и редактировать рекламы
+
 const blockInApp = []; // Токены пользователей, полностью заблокированных в приложении
