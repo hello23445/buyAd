64cd4e24075fbcc5fb431902a57443099c707939
Javad <168000154+hello23445@users.noreply.github.com>
2026-08-08 18:20:49 +0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 /* admin.js */
 
-const ADMIN_TOKENS = ['FQ87GDWKJSLLUVOA', 'L7DE073SCSHV7T8V', '1LJPUBXN5L9PH7S8'];
+const ADMIN_TOKENS = ['VbknQtkspXvnPpW9', 'L7DE073SCSHV7T8V', '1LJPUBXN5L9PH7S8'];
 
 /* ========== ЛОКАЛЬНЫЕ ФЛАГИ (новые) ========== */
 const disableCreateAds = '';// '' или 'disabled'
