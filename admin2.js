4d23d1b66061e414834de9997a48e6463bf8aa4a
Javad <168000154+hello23445@users.noreply.github.com>
2026-08-09 19:50:17 +0400

    Purchases2: Purchases2

--- admin2.js
+++ admin2.js
@@ -6,7 +6,7 @@ export const BlockedUsersIPadresses = [""]; //IP адреса пользоват
 //Общее
 export const CloseApp = ""; // '' или 'closed' — если 'closed' тогда приложение будет закрыто для всех кроме админов.
 export const GASESES = ""; // '' или 'no' — если 'no' тогда блокируем обращения к GAS.
-export const Purchases2 = ""; // '' или 'no' — если 'no' тогда отключаем платежи.
+export const Purchases2 = "no"; // '' или 'no' — если 'no' тогда отключаем платежи.
 export const disableCreateAds2 = ""; // '' или 'disabled' — если 'disabled' тогда запрещаем создавать рекламу.
 export const disableEditingAds = ""; // '' или 'disabled' — если 'disabled' тогда запрещаем редактировать рекламу.
 
