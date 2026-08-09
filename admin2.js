b356d918cb19a2e894497da931aba36b3f860181
Javad <168000154+hello23445@users.noreply.github.com>
2026-08-09 20:06:50 +0400

    Update admin2.js

--- admin2.js
+++ admin2.js
@@ -4,10 +4,10 @@ export const BlockCreateAds = [""]; //Токены пользователей, �
 export const BlockedUsersTelegramID = [""]; //Telegram ID пользователей, которые будут забанены в приложении навсегда.
 export const BlockedUsersIPadresses = [""]; //IP адреса пользователей, которые будут забанены в приложении навсегда.
 //Общее
-export const CloseApp = "not_closed"; // '' или 'closed' — если 'closed' тогда приложение будет закрыто для всех кроме админов.
-export const GASESES = "no"; // '' или 'no' — если 'no' тогда блокируем обращения к GAS.
-export const Purchases2 = "no"; // '' или 'no' — если 'no' тогда отключаем платежи.
-export const disableCreateAds2 = "enabled"; // '' или 'disabled' — если 'disabled' тогда запрещаем создавать рекламу.
-export const disableEditingAds = "enabled"; // '' или 'disabled' — если 'disabled' тогда запрещаем редактировать рекламу.
+export const CloseApp = ""; // '' или 'closed' — если 'closed' тогда приложение будет закрыто для всех кроме админов.
+export const GASESES = ""; // '' или 'no' — если 'no' тогда блокируем обращения к GAS.
+export const Purchases2 = ""; // '' или 'no' — если 'no' тогда отключаем платежи.
+export const disableCreateAds2 = ""; // '' или 'disabled' — если 'disabled' тогда запрещаем создавать рекламу.
+export const disableEditingAds = ""; // '' или 'disabled' — если 'disabled' тогда запрещаем редактировать рекламу.
 
 
